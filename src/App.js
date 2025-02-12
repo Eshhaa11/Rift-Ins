9daa4b82c9a250108797398f43dd2c9cb07853f8
Eshhaa11 <patellesha11@gmail.com>
2025-02-12 10:09:01 +0300

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderComponent from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <HeaderComponent/>
+    </div>
+  );
+}
+
+export default App;
