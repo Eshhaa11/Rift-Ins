438a38c32b986a619340b2536bf80baf1c454f47
Eshhaa11 <patellesha11@gmail.com>
2025-02-16 15:25:22 +0300

    add

---
+++ src/pages/Quote.js
@@ -0,0 +1,10 @@
+const Quote = () => {
+    return (
+      <div>
+        <h1>Get a Quote</h1>
+        <p>Fill out our form to receive a personalized insurance quote.</p>
+      </div>
+    );
+  };
+  
+  export default Quote;
