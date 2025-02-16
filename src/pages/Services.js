d59164231933914b8408ee04a95a39b32cec3c9d
Eshhaa11 <patellesha11@gmail.com>
2025-02-16 15:25:55 +0300

    add

---
+++ src/pages/Services.js
@@ -0,0 +1,10 @@
+const Services = () => {
+    return (
+      <div>
+        <h1>Our Insurance Services</h1>
+        <p>We offer health, car, and life insurance plans.</p>
+      </div>
+    );
+  };
+  
+  export default Services;
