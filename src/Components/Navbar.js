7624b9985c754500eb688bf5a27039c4e748f819
Eshhaa11 <patellesha11@gmail.com>
2025-02-16 15:24:24 +0300

    add

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,18 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-function NavbarComp () {
-    return (
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Insurance Services</a></li>
-                <li><a href="#quote">Get a quote</a></li>
-            </ul>
-        </nav>
-    )
+function NavbarComp() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Insurance Services</a></li>
+        <li><a href="#quote">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavbarComp                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+export default NavbarComp;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
