50a4a76e08620ec01eeda55ec9fdc92946568153
Eshhaa11 <patellesha11@gmail.com>
2025-02-16 15:23:48 +0300

    add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const HeaderComponent = () => {
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#services">Insurance Services</a></li>
-                <li><a href="#quote">Get a Quote</a></li>
+                <li><a href="#quote">Contact Us</a></li>
             </ul>
         </nav>
     )
