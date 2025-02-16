cb94fa3350080b71eb4da84082b0f6398ae75f41
Eshhaa11 <patellesha11@gmail.com>
2025-02-16 15:23:38 +0300

    add

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import NavbarComp from './Components/Header';
 import Hero from './Components/Hero';
 
-
 function App() {
   return (
-     <div>
-     <h1>RIFT INSURANCE 
-    
-      </h1>
-     <NavbarComp/>
-     <Hero />
-     </div>
-  )
-     }
+    <div>
+      <h1>RIFT INSURANCE</h1>
+      <NavbarComp />
+      <Hero />
+    </div>
+  );
+}
 
 export default App;
+
 
