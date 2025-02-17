4396ee53a44c7563f61cb27dec5fe68052c38667
Eshhaa11 <patellesha11@gmail.com>
2025-02-17 11:50:50 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavbarComp from './Components/Header';
 import Hero from './Components/Hero';
+import Features from './Components/Features';
 
 function App() {
   return (
     <div>
-      <h1>RIFT INSURANCE</h1>
+      <h1>RIFT INSURANCE AGENCIES</h1>
       <NavbarComp />
       <Hero />
+      <Features />
     </div>
   );
 }
