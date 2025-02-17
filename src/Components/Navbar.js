f22e075ab174b584ec3bdffacf7880ccb3696887
Eshhaa11 <patellesha11@gmail.com>
2025-02-17 11:51:24 +0300

    add

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ function NavbarComp() {
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Insurance Services</a></li>
-        <li><a href="#quote">Contact Us</a></li>
+        <li><a href="#contact">Contact Us</a></li>
       </ul>
     </nav>
   );
