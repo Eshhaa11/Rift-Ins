6f7830eec8c8d99deefc287baf429716b89b6d3e
Eshhaa11 <patellesha11@gmail.com>
2025-02-19 15:40:31 +0300

    refactor Services component to enhance content and structure

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,18 @@
-const Services = () => {
+import React from "react";
+import "./Services.css";
+
+function Services () {
     return (
       <div>
         <h1>Our Insurance Services</h1>
-        <p>We offer health, car, and life insurance plans.</p>
+        <p>We offer various insurance products, including:</p>
+        <ul>
+          <li>General Insurance Products</li>
+          <li>Life Insurance Products</li>
+          <li>Medical Insurance Products</li>
+        </ul>
       </div>
     );
-  };
+  }
   
   export default Services;
