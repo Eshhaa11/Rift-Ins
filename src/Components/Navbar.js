d9b4653ae5f6d3eca4740c8d4d59dc4025cf405e
Eshhaa11 <patellesha11@gmail.com>
2025-02-19 10:57:48 +0300

    wrap Navbar component in a div for improved layout structure

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,14 +2,17 @@ import "./Navbar.css";
 
 function NavbarComp() {
   return (
+   <div>
     <nav>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Insurance Services</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-      </ul>
+        <ul>
+               <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Insurance Services</a></li>
+                <li><a href="#quote">Contact Us</a></li>
+        </ul>
     </nav>
+    </div>
+
   );
 }
 
