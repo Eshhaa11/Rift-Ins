d030b787aca9589b5116390f42e936385048e4aa
Eshhaa11 <patellesha11@gmail.com>
2025-02-19 10:56:44 +0300

    wrap navigation in a div for improved layout structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,16 @@ import "./Navbar.css"
 
 const HeaderComponent = () => {
     return (
+        <div>
         <nav>
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Insurance Services</a></li>
-                <li><a href="#quote">Contact Us</a></li>
+                   <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Insurance Services</a></li>
+                    <li><a href="#quote">Contact Us</a></li>
             </ul>
         </nav>
+        </div>
     )
 }
 
