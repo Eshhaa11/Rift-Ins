10d9a997dfe9f1cf13ef5ae9e8f5e0b5dd399315
Eshhaa11 <patellesha11@gmail.com>
2025-02-19 10:57:12 +0300

    update Hero.js to change logo image and modify subtitle for branding consistency

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Hero.css"; 
-import heroImage from '../images/logo.png';// Import CSS for Hero component
+import heroImage from '../images/i8.png';// Import CSS for Hero component
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-content">
         <h1>Your Shield in Uncertain Times </h1> 
-        <h2> RIFT INSURANCE </h2>
+        <h2> RIFT NAKURU </h2>
         <p>At Rift Insurance, we believe in securing what matters most. <br /> 
         Trust us to safeguard your future with reliability and care.</p>
         <button className="quote-button">Get a Quote</button>
