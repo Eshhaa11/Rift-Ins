1fa22841ebea8f73469ad2d3712ee0c8d2ba2280
Eshhaa11 <patellesha11@gmail.com>
2025-02-19 15:39:52 +0300

    update About component with detailed agency information and improved structure

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,19 @@
-const About = () => {
-    return (
-      <div>
-        <h1>About Rift Insurance</h1>
-        <p>We provide top-notch insurance solutions tailored for you.</p>
-      </div>
-    );
-  };
-  
-  export default About;
+
+function About () {
+  return (
+    <div className="about">
+      <h1 className="about-heading">OUR AGENCY</h1>
+      <p>Rift Nakuru Insurance Agencies Limited is a registered Insurance Agency < br/>
+      managed by qualified & highly experienced personnel commited to < br/>
+      providing outstanding level of international standard service to our < br/>
+      corporate & individual clientels.< br/>
+      It was founded by Late Niru Shah & Late Haren Shah in 1978</p>
+      <p>Our commitment is to provide quality professional service to our clients. < br/>
+      The Team have a wide experience in providing various insurance policies < br/>
+      to the agriculture, real estate, education, manufacturing and trading sectors.</p>
+      <p>Our Company is duly registered with the Insurance Regulatory Authority < br/>
+      with license renewed up to December 2025</p>
+    </div>
+  );
+}
+
