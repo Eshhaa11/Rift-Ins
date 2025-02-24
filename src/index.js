f7defbeef7a85f2c0d6873b452740470aa742132
Eshhaa11 <patellesha11@gmail.com>
2025-02-24 09:21:25 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Correct import for React 18
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
+import About from './pages/About';
+import Services from './pages/Services';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+
+  {
+    path: "/about",
+    element: <About/>,
+  },
+
+  {
+    path: "/services",
+    element: <Services/>,
+  },
+
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
