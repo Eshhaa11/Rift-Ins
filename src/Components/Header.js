9b691732abe1f8fafb850d90eedd1add0b2a911a
Eshhaa11 <patellesha11@gmail.com>
2025-02-25 10:26:04 +0300

    feat: refactor Header component to use React Router Links for navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,18 @@
-import "./Navbar.css"
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-
-const HeaderComponent = () => {
-    return (
-        <div>
-        <nav>
-            <ul>
-                   <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Insurance Services</a></li>
-                    <li><a href="#quote">Contact Us</a></li>
-            </ul>
-        </nav>
-        </div>
-    )
+const NavbarComp = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link className="nav-link" to="/">Home</Link></li>  
+        <li><Link className="nav-link" to="/about">About Us</Link></li>
+        <li><Link className="nav-link" to="/services">Insurance Services</Link></li>
+        <li><Link className="nav-link" to="/contact">Contact Us</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default HeaderComponent
+export default NavbarComp;
