ef4a04fd6e473464fbb25d5a53a11ed439c07c6b
Eshhaa11 <patellesha11@gmail.com>
2025-02-25 10:25:14 +0300

    feat: enhance About.js with styling and mission statement section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
 
+import React from 'react';
+import './About.css';
+
+
 function About () {
   return (
+    
     <div className="about">
+      <hr className="custom-hr" />
       <h1 className="about-heading">OUR AGENCY</h1>
-      <p>Rift Nakuru Insurance Agencies Limited is a registered Insurance Agency < br/>
-      managed by qualified & highly experienced personnel commited to < br/>
-      providing outstanding level of international standard service to our < br/>
+      <p>Rift Nakuru Insurance Agencies Limited is a registered Insurance Agency
+      managed by < br/>  qualified & highly 
+      experienced personnel commited to 
+      providing outstanding level of < br/> international standard service to our 
       corporate & individual clientels.< br/>
       It was founded by Late Niru Shah & Late Haren Shah in 1978</p>
       <p>Our commitment is to provide quality professional service to our clients. < br/>
@@ -13,6 +20,19 @@ function About () {
       to the agriculture, real estate, education, manufacturing and trading sectors.</p>
       <p>Our Company is duly registered with the Insurance Regulatory Authority < br/>
       with license renewed up to December 2025</p>
+
+      <div className="section-divider"></div>
+
+      
+      <h2 className="misson-statement">MISSION STATEMENT</h2>
+      <p>Rift Nakuru Insurance Agencies Ltd is a professional 
+       company providing < br/> exceptional insurance solutions 
+       through comprehensive risk assessments, < br/> dedicated 
+       customer service, and a commitment to exceeding 
+       expectations,  < br/> 
+       ensuring clients recieve the right 
+       coverage at the most competitive rates.</p>
+
     </div>
   );
 }
