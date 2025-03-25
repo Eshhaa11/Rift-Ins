6a2204e6c35149b484a465c7c298ce52f9b56750
Esha Patel <Patellesha11@gmail.com>
2025-03-25 15:14:50 +0300

    refactor: update iframe style attribute syntax in Contact.js for consistency

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -31,11 +31,10 @@ const Contact = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7682655124263!2d36.08213537447766!3d-0.2870017353492995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18298deb92b6d563%3A0x1db7e4e2fb554957!2sRift%20Nakuru%20Insurance%20Agencies%20Ltd!5e0!3m2!1sen!2ske!4v1742904595965!5m2!1sen!2ske"
                width="600"
                height="450"
-               style="border:0;" 
+               style={{ border: 0 }}
                allowfullscreen="" 
                loading="lazy"
                referrerpolicy="no-referrer-when-downgrade">
-
               </iframe>
 
           </div>
