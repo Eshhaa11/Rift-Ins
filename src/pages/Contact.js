d62c405337e24a73bcb9f041eb1cd16dfc626a5e
Esha Patel <Patellesha11@gmail.com>
2025-03-25 14:16:34 +0300

    refactor: remove onSubmit handler from Contact form for clarity

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
       <i className="fa fa-phone" aria-hidden="true"></i> 
        Phone: +254 722 200 000
      </p>
-     <form className="form" onSubmit={handleSubmit}>
+     <form className="form">
      <p className="title">Get a Quote</p>
      <p className="message">
         Fill in the form below and we will get back to you with a quote.
