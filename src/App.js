bb238ef39c807afd0dfa2b3767bf9a7d06fc1a5c
Esha Patel <Patellesha11@gmail.com>
2025-04-07 14:28:13 +0300

    imported router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"; // Import Router
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Logo from "./images/Logo.png";
 import "./App.css";
 import NavbarComp from "./Components/Header";
 import Hero from "./Components/Hero";
 import Features from "./Components/Features";
+import HomePrev from "./Components/HomePrev";  
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
+// Define App component
 function App() {
   const location = useLocation();  
 
- 
   const backgroundColor = location.pathname === "/" ? "rgb(0, 102, 165)" : "white";
 
   return (
@@ -28,13 +29,24 @@ function App() {
         </h1>
       </div>
 
-      
+      {/* Navbar */}
       <NavbarComp />
 
- 
       <div className="page-container">
         <Routes>
-          <Route path="/" element={<div className="home"><Hero /><Features /></div>} />
+          {/* Homepage Route */}
+          <Route 
+            path="/" 
+            element={
+              <div className="home">
+                <Hero />
+                <Features />
+                <HomePrev />  {/* Add the preview of About, Services, and Contact here */}
+              </div>
+            }
+          />
+
+          {/* Other Routes */}
           <Route path="/about" element={<div className="about-page"><About /></div>} />
           <Route path="/services" element={<div className="services-page"><Services /></div>} />
           <Route path="/contact" element={<div className="contact-page"><Contact /></div>} />
@@ -44,10 +56,10 @@ function App() {
   );
 }
 
-
+// Wrap the entire app with Router
 function AppWrapper() {
   return (
-    <Router>
+    <Router>  {/* Wrap App inside Router */}
       <App />
     </Router>
   );
