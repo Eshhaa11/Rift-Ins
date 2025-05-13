f4129c9536568a7a7cd1c37cc09e2fe00981cb43
Esha Patel <Patellesha11@gmail.com>
2025-05-13 21:55:18 +0300

    Done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,15 +1,17 @@
 import React from "react";
-import "./Hero.css"; 
-import heroImage from '../images/i8.png';
+import "./Hero.css";
+import heroImage from "../images/i8.png";
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Your Shield in Uncertain Times </h1> 
+        <h1>Your Shield in Uncertain Times </h1>
         <h2> RIFT NAKURU INSURANCE AGENCY</h2>
-        <p>At Rift Nakuru , we believe in securing what matters most. <br /> 
-        Trust us to safeguard your future with reliability and care.</p>
+        <p>
+          At Rift Nakuru , we believe in securing what matters most. <br />
+          Trust us to safeguard your future with reliability and care.
+        </p>
       </div>
       <div className="hero-image">
         <img src={heroImage} alt="Hero Section" />
