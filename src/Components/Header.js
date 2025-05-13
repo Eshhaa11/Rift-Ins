20dab1d9745faf0fe7217bd7b13abc43b2d56b40
Esha Patel <Patellesha11@gmail.com>
2025-05-13 21:55:03 +0300

    Done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,13 +6,29 @@ const NavbarComp = () => {
   return (
     <nav className="navbar">
       <ul>
-        <li><Link className="nav-link" to="/">Home</Link></li>  
-        <li><Link className="nav-link" to="/about">About Us</Link></li>
-        <li><Link className="nav-link" to="/services">Insurance Services</Link></li>
-        <li><Link className="nav-link" to="/contact">Contact Us</Link></li>
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/about">
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/services">
+            Insurance Services
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/contact">
+            Contact Us
+          </Link>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default NavbarComp;
