6e5643c94d264ab4251cfca5f361b400337cdbaa
Esha Patel <Patellesha11@gmail.com>
2025-05-13 21:55:46 +0300

    Done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,27 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function NavbarComp() {
   return (
-   <div>
-    <nav>
+    <div>
+      <nav>
         <ul>
-            <Link className="nav-link" to="/home">Home</Link>
-            <Link className="nav-link" to="/about">About</Link>
-            <Link className="nav-link" to="/services">Insurance Services</Link>
-            <Link className="nav-link" to="/contact">Contact Us</Link>
-           
+          <Link className="nav-link" to="/home">
+            Home
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+          <Link className="nav-link" to="/services">
+            Insurance Services
+          </Link>
+          <Link className="nav-link" to="/contact">
+            Contact Us
+          </Link>
         </ul>
-    </nav>
+      </nav>
     </div>
-
   );
 }
 
 export default NavbarComp;
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
