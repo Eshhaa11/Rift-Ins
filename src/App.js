23ac6103851d8b114f3562907594ab5caa047461
Esha Patel <Patellesha11@gmail.com>
2025-05-13 21:54:43 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"; // Import Router
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom"; 
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Logo from "./images/Logo.png";
 import "./App.css";
 import NavbarComp from "./Components/Header";
 import Hero from "./Components/Hero";
 import Features from "./Components/Features";
-import HomePrev from "./Components/HomePrev";  
+import HomePrev from "./Components/HomePrev";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
-
 function App() {
-  const location = useLocation();  
+  const location = useLocation();
 
-  const backgroundColor = location.pathname === "/" ? "rgb(0, 102, 165)" : "white";
+  const backgroundColor =
+    location.pathname === "/" ? "rgb(0, 102, 165)" : "white";
 
   return (
     <div style={{ backgroundColor: backgroundColor, minHeight: "100vh" }}>
-      
       <div className="app-header">
         <img src={Logo} alt="Company Logo" className="app-logo" />
         <h1 className="app-heading">
@@ -29,26 +33,45 @@ function App() {
         </h1>
       </div>
 
-      
       <NavbarComp />
 
       <div className="page-container">
         <Routes>
-          <Route 
-            path="/" 
+          <Route
+            path="/"
             element={
               <div className="home">
                 <Hero />
                 <Features />
-                <HomePrev /> 
+                <HomePrev />
               </div>
             }
           />
 
-          
-          <Route path="/about" element={<div className="about-page"><About /></div>} />
-          <Route path="/services" element={<div className="services-page"><Services /></div>} />
-          <Route path="/contact" element={<div className="contact-page"><Contact /></div>} />
+          <Route
+            path="/about"
+            element={
+              <div className="about-page">
+                <About />
+              </div>
+            }
+          />
+          <Route
+            path="/services"
+            element={
+              <div className="services-page">
+                <Services />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div className="contact-page">
+                <Contact />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </div>
@@ -57,7 +80,7 @@ function App() {
 
 function AppWrapper() {
   return (
-    <Router> 
+    <Router>
       <App />
     </Router>
   );
